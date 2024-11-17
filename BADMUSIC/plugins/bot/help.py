110d433220fca3f86dedd5d877c3fd5382d5566d
Shivmis <githubshiv@gmail.com>
2024-11-17 12:59:59 +0530

    Update help.py

--- BADMUSIC/plugins/bot/help.py
+++ BADMUSIC/plugins/bot/help.py
@@ -479,7 +479,7 @@ async def back_button_callback(client: Client, callback_query: CallbackQuery):
             InlineKeyboardButton(text="🤡 ᴏᴡɴᴇʀ 🤡", user_id=config.OWNER_ID[0]),
             InlineKeyboardButton(
                 text="🗝️ ɢɪᴛʜᴜʙ 🗝️",
-                url="https://github.com/Badhacker98",
+                url="https://github.com/Shivmis/1",
             ),
         ],
         [
